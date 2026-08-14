262a2c9f7f800a377dd5fb3f017e36c14d0fc5af
Nikhil Pandey <nikhilspandey08@gmail.com>
2026-08-14 12:28:18 +0530

    files updated

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import errorHandler from "./middleware/errorHandler.js";
+import authRoutes from "./routes/authRoutes.js";
+import lostRoutes from "./modules/lost/lost.routes.js";
+import foundRoutes from "./modules/found/found.routes.js";
+
+const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/lost", lostRoutes);
+app.use("/api/found", foundRoutes);
+
+app.use(errorHandler);
+export default app;
---
+++ src/utils/generateToken.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import jwt from "jsonwebtoken";
+
+export const generateAccessToken = (admin) => {
+  return jwt.sign(
+    {
+      id: admin.id,
+      email: admin.email,
+    },
+    process.env.JWT_ACCESS_SECRET,
+    {
+      expiresIn: "15m",
+    },
+  );
+};
+
+export const generateRefreshToken = (admin) => {
+  return jwt.sign(
+    {
+      id: admin.id,
+    },
+    process.env.JWT_REFRESH_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+};
